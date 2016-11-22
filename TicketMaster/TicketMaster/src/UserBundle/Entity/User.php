78901f2d8b67c9e0f1025ae6e090693827329bba
urnotwrex <gael.marguiller@supinternet.fr>
2016-11-22 12:23:43 +0100

    add Association Mapping

--- TicketMaster/TicketMaster/src/UserBundle/Entity/User.php
+++ TicketMaster/TicketMaster/src/UserBundle/Entity/User.php
@@ -13,6 +13,20 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class User extends BaseUser
 {
+
+    /**
+     * @ORM\OneToMany(targetEntity="TicketBundle\Entity\TicketEntity", mappedBy="user")
+     */
+
+    private $ticket;
+
+
+    /**
+     * @ORM\OneToMany(targetEntity="CommentBundle\Entity\CommentEntity", mappedBy="user")
+     */
+
+    private $comment;
+
     /**
      * @var int
      *
@@ -22,12 +36,6 @@ class User extends BaseUser
      */
     protected $id;
 
-
-    /**
-     * Get id
-     *
-     * @return int
-     */
 
     public function getId()
     {
@@ -39,5 +47,72 @@ class User extends BaseUser
         parent::__construct();
         // your own logic
     }
-}
 
+    /**
+     * Add ticket
+     *
+     * @param \TicketBundle\Entity\TicketEntity $ticket
+     *
+     * @return User
+     */
+    public function addTicket(\TicketBundle\Entity\TicketEntity $ticket)
+    {
+        $this->ticket[] = $ticket;
+
+        return $this;
+    }
+
+    /**
+     * Remove ticket
+     *
+     * @param \TicketBundle\Entity\TicketEntity $ticket
+     */
+    public function removeTicket(\TicketBundle\Entity\TicketEntity $ticket)
+    {
+        $this->ticket->removeElement($ticket);
+    }
+
+    /**
+     * Get ticket
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getTicket()
+    {
+        return $this->ticket;
+    }
+
+    /**
+     * Add comment
+     *
+     * @param \CommentBundle\Entity\CommentEntity $comment
+     *
+     * @return User
+     */
+    public function addComment(\CommentBundle\Entity\CommentEntity $comment)
+    {
+        $this->comment[] = $comment;
+
+        return $this;
+    }
+
+    /**
+     * Remove comment
+     *
+     * @param \CommentBundle\Entity\CommentEntity $comment
+     */
+    public function removeComment(\CommentBundle\Entity\CommentEntity $comment)
+    {
+        $this->comment->removeElement($comment);
+    }
+
+    /**
+     * Get comment
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getComment()
+    {
+        return $this->comment;
+    }
+}
